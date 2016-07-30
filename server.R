0afc3bf70f3d73eb895662e3257b108b4699ce49
Matt Dickenson <mcdickenson@gmail.com>
2016-07-30 10:25:11 -0600

    Update app

--- server.R
+++ server.R
@@ -6,34 +6,34 @@ distance3d = function(x1, x2, y1, y2, z1, z2){
   return( sqrt( (x1-x2)^2 + (y1-y2)^2 + (z1-z2)^2 ) )
 }
 
-mostSimilarAthlete = function(age, height, weight){
+mostSimilarAthlete = function(age, height, weight, gender){
   similar = NA
   distance = 1000000
-  for(i in 1:nrow(data)){
-    row  = data[i,]
+  subset = data[which(data$gender==gender), ]
+  for(i in 1:nrow(subset)){
+    row  = subset[i,]
     dist = distance3d(age, row['age'], height, row['height'], weight, row['weight'])
     if(dist < distance){
       distance = dist
-      similar = i #row$name
+      similar = i
     }
   }
-  return(similar)
+  return(subset[similar,])
 }
 
 shinyServer(function(input, output){
   output$whoami = renderText({
-    most_similar_ix = mostSimilarAthlete(strtoi(input$myAge), strtoi(input$myHeight), strtoi(input$myWeight))
-    most_similar = data[most_similar_ix, ]
-    paste("<img src='" ,
+    most_similar = mostSimilarAthlete(strtoi(input$myAge), strtoi(input$myHeight), strtoi(input$myWeight), input$myGender)
+    paste("<div style='text-align: center;'><img src='" ,
       most_similar$img,
-      "' width='200'><br /><a href='",
+      "' width='200' style='display: block; margin-left: auto; margin-right: auto;'><br /><a href='",
       most_similar$link,
-      "''>",
+      "'>",
       most_similar$name,
       " (",
       most_similar$sport,
       ")",
-      "</a>",
+      "</a></div>",
       sep="")
   })
 })
--- ui.R
+++ ui.R
@@ -2,15 +2,24 @@ library(shiny)
 
 shinyUI(pageWithSidebar(
 
-  headerPanel("Which member of Team USA are you?"),
+  headerPanel(""),
 
-  sidebarPanel(
-    selectInput("myAge",    label="Age (years)",      choices=seq(from=10, to=99), selected=30),
-    selectInput("myHeight", label="Height (inches)",  choices=seq(from=48, to=84), selected=60),
-    selectInput("myWeight", label="Weight (pounds)",  choices=seq(from=50, to=350), selected=150)
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("myGender", label="Gender",           choices=c("M", "F"),         selected="F"),
+      selectInput("myAge",    label="Age (years)",      choices=seq(from=10, to=99), selected=30),
+      selectInput("myHeight", label="Height (inches)",  choices=seq(from=48, to=84), selected=60),
+      selectInput("myWeight", label="Weight (pounds)",  choices=seq(from=50, to=350), selected=150),
+      width=4
+    ),
+
+    mainPanel(
+      htmlOutput("whoami"),
+      width=8
+    )
   ),
 
-  mainPanel(
-    htmlOutput("whoami")
-  )
+  mainPanel()
+
+
 ))
